c99e22e552f8a0d01d10a76eb5bcc52c85f719ab
Your Name <you@example.com>
2022-08-31 10:26:20 +0530

    logic for total working hour

---
+++ src/EmployeeWage.java
@@ -0,0 +1,41 @@
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        int wagePerHour = 20;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
+        int dailyWage = 0;
+        int totalWorkingDays = 0;
+        int totalHour = 0;
+        int totalDailyWage = 0;
+        while (totalHour < 80 && totalWorkingDays < 20) {
+            int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+
+            totalWorkingDays ++;
+            switch (employeeCheck) {
+                case 2:
+                    dailyWage = fullTimeHour * wagePerHour;
+                    System.out.println("Employee is present full time");
+                    totalHour =  totalHour + fullTimeHour;
+                    totalDailyWage = totalDailyWage + fullTimeHour * wagePerHour;
+                    System.out.println("Employee is present full time: " + totalDailyWage);
+                    break;
+                case 1:
+                    dailyWage = partTimeHour * wagePerHour;
+                    System.out.println("Employee is present half time");
+                    totalHour =  totalHour + partTimeHour;
+                    totalDailyWage = totalDailyWage + partTimeHour * wagePerHour;
+                    System.out.println("Employee is present half time: " + totalDailyWage);
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    System.out.println("Employee is absent: " + totalDailyWage);
+            }
+            System.out.println("Working Days is: " + totalWorkingDays + " Hours: " + totalHour);
+
+            System.out.println("Employee total wage is: " + dailyWage);
+        }
+
+        System.out.println("Employee total wage for month is: " + totalDailyWage);
+    }
+}
